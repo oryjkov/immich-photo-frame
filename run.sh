3847dec0a7d60868b192b3e472fcc8dbd19b15b2
Oleg Ryjkov <oryjkov@gmail.com>
2024-06-14 15:41:16 +0200

    API key does not need to be hardcoded any more

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-(set -a ; source .env && envsubst '$API_KEY' < nginx/nginx.conf)  > run/nginx.conf && podman run  --network host  -v $(pwd)/dist:/data/frame -v $(pwd)/run/nginx.conf:/etc/nginx/nginx.conf:ro docker.io/nginx
+podman run  --network host \
+  -v $(pwd)/dist:/data/photo-frame \
+  -v $(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+  docker.io/nginx
