e42bf860f56f4f6e7b957c61146890aa71acd04d
Oleg Ryjkov <oryjkov@gmail.com>
2024-06-17 09:11:55 +0200

    run script update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Runs an nginx reverse proxy to serve the local changes with an
+# instance of immich at `immich.local` - see nginx/nginx-dev.conf
 podman run  --network host \
   -v $(pwd)/dist:/data/photo-frame \
-  -v $(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v $(pwd)/nginx/nginx-dev.conf:/etc/nginx/nginx.conf:ro \
   docker.io/nginx
